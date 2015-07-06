1febedc65691c8acc4f7e2361f87aea9e7f575a0
gfoster <gfoster@ulive.com>
2015-07-06 12:32:13 -0700

    Add basic service support

--- ambari_client.rb
+++ ambari_client.rb
@@ -1,4 +1,5 @@
 require 'rest_client'
+require 'byebug'
 
 class AmbariCluster
 
@@ -17,14 +18,44 @@ class AmbariCluster
   end
 
   def clusters
+    # Fetch a list of clusters this ambari server manages
+    # returns an array
     res = JSON(RestClient.get(@uri + "clusters/"))['items']
     clusters = res['items'].collect { |item| item["Clusters"]["cluster_name"]}
     return clusters
   end
 
+  def cluster(cluster)
+    # Fetch a JSON object describing the named cluster
+    res = JSON(RestClient.get(@uri + "clusters/#{cluster}"))
+    return res
+  end
+
   def hosts(cluster)
+    # Fetch a list of hosts within a named cluster
+    # returns an array
     res = JSON(RestClient.get(@uri + "clusters/#{cluster}/hosts/"))
     hosts = res['items'].collect { |item| item["Hosts"]["host_name"]}
+    return hosts
+  end
+
+  def host(cluster, host)
+    # Fetch a JSON object describing the named host
+    res = JSON(RestClient.get(@uri + "clusters/#{cluster}/hosts/#{host}/"))
+    return res
+  end
+
+  def services(cluster)
+    # Fetch a list of services within a named cluster
+    res = JSON(RestClient.get(@uri + "clusters/#{cluster}/services/"))
+    services = res['items'].collect { |item| item["ServiceInfo"]["service_name"]}
+    return services
+  end
+
+  def service(cluster, service)
+    # Fetch a JSON object describing the names service
+    res = JSON(RestClient.get(@uri + "clusters/#{cluster}/services/#{service}"))
+    return res
   end
 end
 
